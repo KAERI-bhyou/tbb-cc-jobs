14f81b13d4b8e55154ff3a05f4701e97a4dcfd64
Byung-Hyun You <bhyou@kaeri.re.kr>
2023-01-27 23:22:57 +0900

    `tbb::parallel_for_each` example

--- include/mod/mod.hpp
+++ include/mod/mod.hpp
@@ -1,12 +1,13 @@
 #ifndef B165F7C4_5C9A_4D64_AB4D_6593AC2A743B
 #define B165F7C4_5C9A_4D64_AB4D_6593AC2A743B
 
+#include <vector>
 #include <tbb/parallel_for.h>
 #include <tbb/concurrent_priority_queue.h>
 
 class Mod
 {
-    tbb::concurrent_priority_queue<int, std::greater<int>> queue;
+    std::vector<int> is;
 
 public:
     Mod() = default;
--- src/mod/mod.cpp
+++ src/mod/mod.cpp
@@ -1,12 +1,32 @@
+#include <thread>
+#include <random>
+#include <chrono>
 #include <iostream>
+#include <tbb/parallel_for_each.h>
+#include <tbb/task_arena.h>
+#include <fmt/format.h>
 
 #include "mod/mod.hpp"
 
 void Mod::run()
 {
-    tbb::parallel_for( 0, 20, [ & ]( int i ) { queue.push( i ); } );
+    for( int i = 0; i < 100; ++i )
+        is.push_back( i );
 
-    int num = 0;
-    while( queue.try_pop( num ) )
-        std::cout << num << " ";
+    std::mt19937_64 eng{ std::random_device{}() }; // or seed however you want
+    tbb::task_arena arena( 4 );
+
+    arena.execute(
+        [ & ]
+        {
+            tbb::parallel_for_each( std::begin( is ), std::end( is ),
+                                    [ & ]( int ii )
+                                    {
+                                        std::uniform_int_distribution<> dist{ 1, 5 };
+                                        auto sec = dist( eng );
+                                        int worker_index = tbb::this_task_arena::current_thread_index();
+                                        std::this_thread::sleep_for( std::chrono::seconds{ sec } );
+                                        std::cout << fmt::format( "{:2}: sleeped {} sec(s) at [{}]\n", ii, sec, worker_index );
+                                    } );
+        } );
 }
